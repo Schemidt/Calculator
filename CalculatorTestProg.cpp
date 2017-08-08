7b85b3eba20b5ad29157205c8ec290365f728f0e
Alexey <alexeySamson@yandex.ru>
2017-08-08 17:53:13 +0300

    updated

--- CalculatorTestProg.cpp
+++ CalculatorTestProg.cpp
@@ -2,53 +2,55 @@
 
 int main(void)
 {
-	if (calculator("1+2\n") == 3)//1+2=3
+	double res=0;
+
+	if (res=calculator("1+2\n") == 3)//1+2=3
 		printf("1+2 = 3 - OK\n");
 	else
-		printf("1+2 != 3 - FAULT\n");
+		printf("1+2 != %f - FAULT\n",res);
 
-	if (calculator("3-1\n") == 2)//3-1=2
+	if (res = calculator("3-1\n") == 2)//3-1=2
 		printf("3-1 = 2 - OK\n");
 	else
-		printf("3-1 != 2 - FAULT\n");
+		printf("3-1 != %f - FAULT\n", res);
 
-	if (calculator("3*1\n") == 3)//3*1=3
+	if (res = calculator("3*1\n") == 3)//3*1=3
 		printf("3*1 = 3 - OK\n");
 	else
-		printf("3*1 != 3 - FAULT\n");
+		printf("3*1 != %f - FAULT\n", res);
 
-	if (calculator("1/2\n") == 0.5)//1/2=0.5
+	if (res = calculator("1/2\n") == 0.5)//1/2=0.5
 		printf("1/2 = 0.5 - OK\n");
 	else
-		printf("1/2 != 0.5 - FAULT\n");
+		printf("1/2 != %f - FAULT\n", res);
 
-	if (calculator("(1)\n") == 1)//(1)=1
+	if (res = calculator("(1)\n") == 1)//(1)=1
 		printf("(1) = 1 - OK\n");
 	else
-		printf("(1) != 1 - FAULT\n");
+		printf("(1) != %f - FAULT\n", res);
 
-	if (calculator("(-1)\n") == -1)//(-1)=-1
+	if (res = calculator("(-1)\n") == -1)//(-1)=-1
 		printf("(-1) = -1 - OK\n");
 	else
-		printf("(-1) = -1 - FAULT\n");
+		printf("(-1) = %f - FAULT\n", res);
 
-	if (calculator(" - 1 + 5 - 3\n") == 1)//-1 + 5 - 3 = 1
+	if (res = calculator(" - 1 + 5 - 3\n") == 1)//-1 + 5 - 3 = 1
 		printf("-1 + 5 - 3 = 1 - OK\n");
 	else
-		printf("-1 + 5 - 3 != 1 - FAULT\n");
+		printf("-1 + 5 - 3 != %f - FAULT\n", res);
 
-	if (calculator("-10 + (8 * 2.5) - (3 / 1, 5)\n") == 8)//-10 + (8 * 2.5) - (3 / 1, 5) = 8
+	if (res = calculator("-10 + (8 * 2.5) - (3 / 1, 5)\n") == 8)//-10 + (8 * 2.5) - (3 / 1, 5) = 8
 		printf("-10 + (8 * 2.5) - (3 / 1, 5) = 8 - OK\n");
 	else
-		printf("-10 + (8 * 2.5) - (3 / 1, 5) != 8 - FAULT\n");
+		printf("-10 + (8 * 2.5) - (3 / 1, 5) != %f - FAULT\n", res);
 
-	if (calculator("1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5)\n") == 11)//1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5) = 11
+	if (res = calculator("1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5)\n") == 11)//1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5) = 11
 		printf("1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5) = 11 - OK\n");
 	else
-		printf("1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5) != 11 - FAULT\n");
+		printf("1 + (2 * (2.5 + 2.5 + (3 - 2))) - (3 / 1.5) != %f - FAULT\n", res);
 
 
-
+	getchar();
 	return 0;
 }
 
